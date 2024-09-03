462d0e250f71652dca79a29870e4504731e58d9d
Muzi PH <muzi.phage@gmail.com>
2024-09-03 16:01:06 +0200

    using dependency array

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
-// useEffect takes two arguments, the function that will be called when we want the effect to happen
-// second argument is the field to listen for any changes that will trigger the effect like changes in emotion below
+  const [secondary, setSecondary] = useState("tired");
+  
   useEffect(() => {
     console.log(`It's ${emotion} right now`);
-  },[emotion]);
+  },[emotion, secondary]); // Listen to changes in the dependency array
+  
+  useEffect(() => {
+    console.log(`It's ${secondary} around here`)
+  }, [secondary]);
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
       <button onClick={() => setEmotion("sad")} >Sad</button>
       <button onClick={() => setEmotion("excited")} >Excited</button>
+      <h2>Current secondary emotion is {secondary}</h2>
+      <button onClick={() => setSecondary("grateful")}>Grateful</button>
     </div>
   );
 }
