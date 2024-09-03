bda4fc59f0f7974b86c4ef446f7eeca3354471b2
Muzi PH <muzi.phage@gmail.com>
2024-09-03 15:34:26 +0200

    useState Hook to update state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
-const cities = ["Jozi", "Durban", "Cape Town"];
-console.log(cities[0]);
-
-// Destructuring array above and using position to refer to array elements
-const [first, second] = ["Jozi", "Durban", "Cape Town"];
-console.log(second);
-console.log(first);
-function App({ library }) {
+function App() {
+  // useState takes two parameters, a value for initial state and a function to update the state
+  // We used destructuring to capture the initial value emotion and the function to update emotion state, setEmotion
+  const [emotion, setEmotion] = useState("what");
   return (
     <div className="App">
-      <h1>Hello from {library}</h1>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")} >Sad</button>
+      <button onClick={() => setEmotion("excited")} >Excited</button>
     </div>
   );
 }
