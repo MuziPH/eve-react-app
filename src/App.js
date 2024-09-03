7ec37b142c48e793d38c26ba82b82a5cfe74892a
Muzi PH <muzi.phage@gmail.com>
2024-09-03 16:20:00 +0200

    useReducer to externalize the funtion to update state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useReducer } from 'react';
 import './App.css';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-  
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`);
-  },[emotion, secondary]); // Listen to changes in the dependency array
-  
-  useEffect(() => {
-    console.log(`It's ${secondary} around here`)
-  }, [secondary]);
+  // useReducer takes a function to update and second argument i sthe value to update
+  const [checked, setChecked] = useReducer(checked => !checked, false);
   return (
     <div className="App">
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")} >Sad</button>
-      <button onClick={() => setEmotion("excited")} >Excited</button>
-      <h2>Current secondary emotion is {secondary}</h2>
-      <button onClick={() => setSecondary("grateful")}>Grateful</button>
+      <input
+        type='checkbox'
+        value={checked}
+        onChange={setChecked}
+      />
+      <label>{checked ? "Checked" : "Not Checked"}</label>
     </div>
   );
 }
