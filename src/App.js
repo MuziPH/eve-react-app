628ebd5b7e53cea279d1ed259c813661e05deb07
Muzi PH <muzi.phage@gmail.com>
2024-09-07 11:50:47 +0200

    Handling loading states

--- src/App.js
+++ src/App.js
@@ -6,27 +6,36 @@ function GitHubUser({ name, location, avatar }) {
     <div>
       <h1>{name}</h1>
       <p>{location}</p>
-      <img src={avatar} height={150} alt="Github user"/>
+      <img src={avatar} height={150} alt="Github user" />
     </div>
   );
 }
 function App() {
   // Create a container for data
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     fetch(`https://api.github.com/users/muziph`)
       .then((response) => response.json())
-      .then((data) => setData(data));
+      .then((data) => setData(data))
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
-  if (data)
-    return (
-      <GitHubUser
-        name={data.name}
-        location={data.location}
-        avatar={data.avatar_url}
-      />
-    );
-  return <h1>Data</h1>;
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  if (!data) return null;
+
+  return (
+    <GitHubUser
+      name={data.name}
+      location={data.location}
+      avatar={data.avatar_url}
+    />
+  );
 }
 
 export default App;
