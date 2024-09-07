5d1ab4af2f045718c7d7540268deb5213adbde8c
Muzi PH <muzi.phage@gmail.com>
2024-09-07 11:43:19 +0200

    Fetch and display data from API

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-// Custom hook encapsulating common functionality of taking an initial value and updating it to an event target value
-function useInput(initialValue) {
-  const [value, setValue] = useState(initialValue);
-  return [
-    { value, onChange: event => setValue(event.target.value) },
-    () => setValue(initialValue)
-  ];
+function GitHubUser({ name, location, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt="Github user"/>
+    </div>
+  );
 }
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("");
-
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value}, ${colorProps.value}`);
-    resetTitle();
-    resetColor();
-  };
-  return (
-    <form onSubmit={submit}>
-      <input
-        {...titleProps}
-        type="text"
-        placeholder="color title..."
+  // Create a container for data
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/muziph`)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+  if (data)
+    return (
+      <GitHubUser
+        name={data.name}
+        location={data.location}
+        avatar={data.avatar_url}
       />
-      <input
-        {...colorProps}
-        type="color"
-      />
-      <button>ADD</button>
-    </form>
-  );
+    );
+  return <h1>Data</h1>;
 }
 
 export default App;
