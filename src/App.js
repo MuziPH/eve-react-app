b313c858394b3be8626d948f39f81345577870ed
Muzi PH <muzi.phage@gmail.com>
2024-09-07 09:56:51 +0200

    Building a custom hook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import { useState } from "react";
 import "./App.css";
 
+// Custom hook encapsulating common functionality of taking an initial value and updating it to an event target value
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  return [
+    { value, onChange: event => setValue(event.target.value) },
+    () => setValue(initialValue)
+  ];
+}
 function App() {
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("");
 
   const submit = (e) => {
     e.preventDefault();
-    alert(`${title}, ${color}`);
-    setTitle("");
-    setColor("");
+    alert(`${titleProps.value}, ${colorProps.value}`);
+    resetTitle();
+    resetColor();
   };
   return (
     <form onSubmit={submit}>
       <input
-        value={title}
-        onChange={(event) =>
-          setTitle(event.target.value)
-        }
+        {...titleProps}
         type="text"
         placeholder="color title..."
       />
       <input
-        value={color}
-        onChange={(event) =>
-          setColor(event.target.value)
-        }
+        {...colorProps}
         type="color"
       />
       <button>ADD</button>
