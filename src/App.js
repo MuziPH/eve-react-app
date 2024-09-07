383b86dfb67b801278af8346f8238a73e8db23f1
Muzi PH <muzi.phage@gmail.com>
2024-09-07 09:37:22 +0200

    Controlled components with useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import { useRef } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  // useRef retrieves values from the the form, like which color is selected
-  // retrives the value of text in the input
-  const txtTitle = useRef();
-  const hexColor = useRef();
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("");
 
   const submit = (e) => {
     e.preventDefault();
-    const title = txtTitle.current.value;
-    const color = hexColor.current.value;
-    alert(`${title}, ${color}`)
-    txtTitle.current.value="";
-    hexColor.current.value="";
+    alert(`${title}, ${color}`);
+    setTitle("");
+    setColor("");
   };
   return (
     <form onSubmit={submit}>
-      <input ref={txtTitle} type='text' placeholder='color title...' />
-      <input ref={hexColor} type='color' />
+      <input
+        value={title}
+        onChange={(event) =>
+          setTitle(event.target.value)
+        }
+        type="text"
+        placeholder="color title..."
+      />
+      <input
+        value={color}
+        onChange={(event) =>
+          setColor(event.target.value)
+        }
+        type="color"
+      />
       <button>ADD</button>
     </form>
   );
